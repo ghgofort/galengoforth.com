aa7dabe77b48a35d0aaa159e9ad8de6f20ae48dc
ghgofort <galengoforth@gmail.com>
2023-09-13 15:19:55 -0400

    develop: Updates to graphics & firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import AboutMe from './components/aboutMe';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <AboutMe />
       </header>
     </div>
