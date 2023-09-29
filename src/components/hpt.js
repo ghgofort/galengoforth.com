26c90abe643a67a77e99b9575b2fca11932a7c60
ghgofort <galengoforth@gmail.com>
2023-09-29 14:42:31 -0400

    Updates to pull data from API & basic formatting on the page.

--- src/components/hpt.js
+++ src/components/hpt.js
@@ -7,8 +7,9 @@ import React, { useState, useEffect } from 'react';
 import './hpt.css';
 
 const getHPT = async () => {
-    const response = await fetch('https://pi-services-59qvrknrb-galen-goforths-projects.vercel.app/hpt');
+    const response = await fetch('https://pi-services.vercel.app/hpt');
     const body = await response.json();
+    console.log(body);
 
     if (response.status !== 200) {
         throw Error(body.message);
@@ -18,17 +19,30 @@ const getHPT = async () => {
 };
 
 function HPT() {
-    const [apiResponse, setApiResponse] = useState("*** now loading ***");
+    const [apiResponse, setApiResponse] = useState([]);
     useEffect(() => {
         getHPT()
-            .then(res => res.text())
             .then(res => setApiResponse(res))
             .catch(err => err);
     }, []);
 
     return (
         <div className="hpt__component">
-            {apiResponse}
+            <h4>Humidity, Pressure, & Temperature Readings from Raspberry Pi server.</h4>
+            {apiResponse.map((item, index) => (
+                <div key={index}>
+                    <h5>Date / Time: {new Date((item.dateTimeCreated.seconds * 1000)).toDateString()}</h5>
+                    <p className="hpt__slide">
+                        Humidity: {item.humidity}
+                    </p>
+                    <p className="hpt__slide">
+                        Pressure: {item.pressure_inHg}
+                    </p>
+                    <p className="hpt__slide">
+                        Temperature (f): {item.temperature_F}
+                    </p>
+                </div>
+            ))}
         </div>
     );
 };
