4c369be768cd5f4ec4ea627333fa58396b1be45e
ghgofort <galengoforth@gmail.com>
2023-09-29 22:00:03 -0400

    fix hpt to max 10 items & add missing time + formatting

--- src/components/hpt.js
+++ src/components/hpt.js
@@ -22,27 +22,46 @@ function HPT() {
     const [apiResponse, setApiResponse] = useState([]);
     useEffect(() => {
         getHPT()
-            .then(res => setApiResponse(res))
-            .catch(err => err);
+            .then(res => {
+                // Only show the last 10 readings.
+                res = res.slice(Math.max(res.length - 10, 0));
+                setApiResponse(res);
+            }).catch(err => err);
     }, []);
 
     return (
         <div className="hpt__component">
             <h4>Humidity, Pressure, & Temperature Readings from Raspberry Pi server.</h4>
-            {apiResponse.map((item, index) => (
-                <div key={index}>
-                    <h5>Date / Time: {new Date((item.dateTimeCreated.seconds * 1000)).toDateString()}</h5>
-                    <p className="hpt__slide">
-                        Humidity: {item.humidity}
-                    </p>
-                    <p className="hpt__slide">
-                        Pressure: {item.pressure_inHg}
-                    </p>
-                    <p className="hpt__slide">
-                        Temperature (f): {item.temperature_F}
-                    </p>
-                </div>
-            ))}
+            {
+                apiResponse.map((item, index) => {
+                    const date = new Date((item.dateTimeCreated.seconds * 1000));
+                    let time = '';
+                    let isAM = true;
+                    if (date.getHours() > 12) {
+                        time = date.getHours() - 12;
+                        isAM = false;
+                    } else if (date.getHours() === 0) {
+                        time = 12;
+                    }
+                    time = time + ':' + (date.getMinutes() < 10 ? '0' +
+                        date.getMinutes() : date.getMinutes()) + ' ' +
+                        (isAM ? 'AM' : 'PM');
+                    return (
+                        <div key={index}>
+                            <p><strong>Date: </strong>{date.toDateString()} <strong>Time:</strong> {time}</p>
+                            <p className="hpt__slide">
+                                Humidity: {item.humidity}
+                            </p>
+                            <p className="hpt__slide">
+                                Pressure: {item.pressure_inHg}
+                            </p>
+                            <p className="hpt__slide">
+                                Temperature (f): {item.temperature_F}
+                            </p>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 };
