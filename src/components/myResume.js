080a74083db4c53e5b159a2becfa93d8b6556fc1
ghgofort <galengoforth@gmail.com>
2023-10-11 11:44:16 -0400

    add separate resume component & updates to move certs out of aboutMe component.

---
+++ src/components/myResume.js
@@ -0,0 +1,40 @@
+/**
+ * React component to render my resume in an interactive format.
+ * @module src/components/myResume
+ */
+
+import { useState } from "react";
+import './myResume.css';
+
+import sfccArchitect from '../images/sfcc-architect-cert.png';
+import sfccDeveloper from '../images/sfcc-developer-cert.png';
+
+function MyResume() {
+    const [currentSection, setCurrentSection] = useState('introduction');
+
+    return (
+        <div className="MyResume__component">
+            <h1>Galen Goforth - Software Engineer</h1>
+            <div id="introduction" className="MyResume__introduction">
+                <h2>Introduction</h2>
+                <p>Hi, I'm <span className="MyResume__name">Galen</span>! I'm a software engineer with a passion for learning and creating.</p>
+            </div>
+            <div className="MyResume__Certifications">
+                <h2>Certifications</h2>
+                <ul>
+                    <li><p>Salesforce Commerce Cloud (SFCC) Technical Leadership & Experience</p>
+                    </li>
+                    <li>
+                        <span>Salesforce Certified B2C Commerce Developer & Technical Architect</span>
+                        <div>
+                            <img className="AboutMe__cert-img" src={sfccDeveloper} alt={'Salesforce B2C Commerce Developer Certification Badge'}/>
+                            <img className="AboutMe__cert-img" src={sfccArchitect} alt={'Salesforce B2C Commerce Technical Architect Certification Badge'}/>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default MyResume;
