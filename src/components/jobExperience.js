80d65a3f221a25ea03304640273e7928b97b09d7
ghgofort <galengoforth@gmail.com>
2023-10-16 13:19:53 -0400

    Added new jobExperience component to use for each job experience & possibly school &/or certs as well.

---
+++ src/components/jobExperience.js
@@ -0,0 +1,31 @@
+/** 
+ * React component for a creative way to display my past work & other relevant
+ * experience in a digital format. 
+ */
+
+import './jobExperience.css';
+
+const JobExperience = (props) => {
+    return (
+        <div className="job-experience__component">
+            <h3>{props.expData.title}</h3>
+            <h4>{props.expData.companyName}</h4>
+            <h5>{props.expData.companyCity + ', ' + props.expData.companyState}</h5>
+            <h5>{props.startDateMonth + '/' + props.startDateYear + ' - ' + props.endDateMonth + '/' + props.endDateYear}</h5>
+            <p>{props.expData.description}</p>
+            <div className="job-experience__component__examples">
+                {props.expData.highlights.map((highlight, index) => {
+                    let key = index;
+                    return (
+                        <div key={key} className="job-experience__component__highlight">
+                            <img src={highlight.img} alt={highlight.alt} />
+                            <p>{highlight.description}</p>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default JobExperience;
